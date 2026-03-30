b139f887d3823b95105f01316144bcc7c01e1a42
wer9808 <60593364+wer9808@users.noreply.github.com>
2026-03-30 09:30:43 +0900

    feat: 투두 컨테이너 헤더 영역 실시간 시계 구현 (#4)
    * feat: 투두 컨테이너 헤더 영역 실시간 시계 구현
    
    * 사용하지 않는 변수 제거
    
    * chore: 마운트 시 타이머 시작
    
    - 마운트 이후 타이머가 시작되도록 변경
    - 시간, 날짜 출력 문자열 값이 computed로 계산되도록 변경

--- src/components/todo/TodoHeader.vue
+++ src/components/todo/TodoHeader.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { computed, onMounted, onUnmounted, ref } from "vue";
+
 const emit = defineEmits(["change-filter"]);
 
 const { currentFilter, filters } = defineProps({
@@ -6,14 +8,40 @@ const { currentFilter, filters } = defineProps({
   filters: Array,
 });
 
+const now = ref(new Date());
+
+const timeString = computed(() => now.value.toLocaleTimeString());
+const dateString = computed(() => now.value.toLocaleDateString());
+
 const changeFilter = (id) => {
   emit("change-filter", id);
 };
+
+// 시간 갱신 타이머 핸들
+let clockInterval;
+
+// 마운트 시 타이머 시작
+onMounted(() => {
+  clockInterval = setInterval(() => {
+    now.value = new Date();
+  }, 1000);
+});
+
+// 언마운트 시 타이머 해제
+onUnmounted(() => {
+  clearInterval(clockInterval);
+});
 </script>
 
 <template>
   <div class="todo-header">
-    <div class="title">TodoTodo</div>
+    <div class="title-container">
+      <div class="title">TodoTodo</div>
+      <div class="datetime-box">
+        <div class="date">{{ dateString }}</div>
+        <div class="clock">{{ timeString }}</div>
+      </div>
+    </div>
     <ul class="todo-filter-tab">
       <li
         v-for="filter in filters"
@@ -34,11 +62,35 @@ const changeFilter = (id) => {
   flex-direction: column;
 }
 
-.todo-header > .title {
+.title-container {
   margin: 16px;
+  flex: 0 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: end;
+}
+
+.title {
   font-size: 3rem;
   font-weight: bold;
   font-family: Arial, Helvetica, sans-serif;
+}
+
+.datetime-box {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: end;
+}
+
+.datetime-box > .date {
+  font-size: 1.2rem;
+}
+
+.datetime-box > .clock {
+  font-size: 2rem;
 }
 
 .todo-filter-tab {
