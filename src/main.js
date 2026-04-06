7bdbf17c73187d960351440c5c0a306f87e08a87
wer9808 <60593364+wer9808@users.noreply.github.com>
2026-04-06 15:19:46 +0900

    feat: pinia 라이브러리 도입 (#24)
    pinia 라이브러리를 통해 공유 상태 모듈을 pinia store를 이용한 모듈로 재작성했습니다.
    
    작업 모듈 : src/stores/useTodoStore.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/main.css";
 import "@/assets/style.css";
+import { createPinia } from "pinia";
 import appRouter from "./router";
 
 const app = createApp(App);
+app.use(createPinia());
 app.use(appRouter);
 app.mount("#app");
